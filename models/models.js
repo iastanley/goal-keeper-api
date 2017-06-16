fefa3646c02d95f3fbb523a27287f2e893a684fd
iastanley <iasvks.craigslist@gmail.com>
2017-06-16 13:34:38 -0700

    added models for database

---
+++ models/models.js
@@ -0,0 +1,41 @@
+'use strict';
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+mongoose.Promise = global.Promise;
+
+// MODEL FOR GOALS
+// not sure how to represent tasks right now
+const goalSchema = mongoose.Schema({
+  user: String,
+  title: String,
+  color: String,
+  tasks: {}
+});
+
+const Goal = mongoose.model('Goal', goalSchema);
+
+// MODEL FOR USERS
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+userSchema.methods.validatePassword = function(password) {
+  return bcrypt.compare(password, this.password);
+}
+
+userSchema.statics.hashPassword = function(password) {
+  return bcrypt.hash(password, 10);
+}
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = {Goal, User};
