8fdd3594b5a7759bed0884608ad7555e9befba25
iastanley <iasvks.craigslist@gmail.com>
2017-06-16 15:34:28 -0700

    added POST route for new goals

--- routers/goalRouter.js
+++ routers/goalRouter.js
@@ -9,17 +9,52 @@ mongoose.Promise = global.Promise;
 const { Goal } = require('../models/models');
 router.use(bodyParser.json());
 
+let currentUser = "Illy";
+// GET all goals
 router.get('/', (req, res) => {
   Goal
-    .find()
+    .find({user: currentUser})
     .exec()
     .then(goals => {
       res.status(200).json(goals);
     })
     .catch(err => {
       console.error(err);
-      res.status(500).json({message: 'Internal server error'});
+      res.status(500).json({message: 'Internal server error after goals GET route'});
     })
+});
+
+// POST new goal to server and respond with new goal
+router.post('/', (req, res) => {
+  const requiredFields = ['title', 'color'];
+  requiredFields.forEach(field => {
+    if (!(field in req.body)) {
+      const message = `Missing '${field}' in request body`;
+      console.error(message);
+      return res.status(400).json({message: message});
+    }
+  });
+
+  let newData = {
+    user: currentUser,
+    title: req.body.title,
+    color: req.body.color,
+    tasks: {}
+  }
+
+  //if a new task is created with a new goal it is always the first task at key 0
+  if (req.body.task) {
+    newData.tasks[0] = req.body.task;
+  }
+
+  Goal
+    .create(newData)
+    .then(goal => {
+      res.status(201).json(goal);
+    })
+    .catch(err => {
+      res.status(500).json({message: 'Internal server error after goals POST route'});
+    });
 });
 
 module.exports = router;
